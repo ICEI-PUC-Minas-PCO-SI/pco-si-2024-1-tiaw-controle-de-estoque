5f5a165674bdc0e0b8af38f8ff8e062013754690
Iago Moysés <iagogmoyses21@gmail.com>
2024-06-30 18:20:55 -0300

    Tentando vercel

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const jsonServer = require('json-server');
-const express = require('express');  // Adicione esta linha para importar o módulo express
+const express = require('express');
 const path = require('path');
 
 const server = jsonServer.create();
@@ -10,10 +10,15 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 // Configure o roteador do json-server
-server.use(router);
+server.use('/api', router);  // Adicione um prefixo para os endpoints da API
 
 // Serve arquivos estáticos HTML
 server.use(express.static(path.join(__dirname, 'codigo/html')));
+
+// Serve a página inicial (index.html) se não houver outras rotas
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'codigo/html', 'index.html'));
+});
 
 // Inicia o servidor na porta especificada ou na porta 3000 por padrão
 server.listen(process.env.PORT || 3000, () => {
