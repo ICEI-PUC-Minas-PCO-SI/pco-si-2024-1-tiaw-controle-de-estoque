ef58d7965e3afbe17f035adacc9acd1d20f47707
Iago Moysés <iagogmoyses21@gmail.com>
2024-06-30 16:54:24 -0300

    Json voltando a funcionar no vercel

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('codigo/assets/javascript/Json/login.json');
-const middlewares = jsonServer.defaults();
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
 
-server.use(middlewares);
-server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running')
+app.use(express.static(path.join(__dirname, 'codigo')));
+
+// API endpoint para servir JSON
+app.get('/api/json', (req, res) => {
+  const jsonFilePath = path.join(__dirname, 'codigo/assets/javascript/Json/login.json');
+  fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Erro ao ler o arquivo JSON');
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
 });
+
+// Definindo a porta para o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+module.exports = app;
