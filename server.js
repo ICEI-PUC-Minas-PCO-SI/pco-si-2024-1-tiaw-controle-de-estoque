79be133444d5565de0f43536fd12f28d3eae21db
Iago Moysés <iagogmoyses21@gmail.com>
2024-06-30 18:12:33 -0300

    TENTANDO FAZER Json voLTAR a funcionar no vercel

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+server.use(jsonServer.static('codigo/html'));
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running on port', process.env.PORT || 3000);
 });
